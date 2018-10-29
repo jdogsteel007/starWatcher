6050594b16d7302f12d3aa1795cbf76171b16e75
Joe Mazero <jmazero@umail.iu.edu>
2018-10-29 12:48:32 -0400

    start

---
+++ starWatcher/Assets/Script/star.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class star : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter()
+    {
+        print(gameObject.name);
+        gameObject.transform.localScale += new Vector3(.45f, .45f, 0);
+    }
+
+    private void OnMouseExit()
+    {
+        gameObject.transform.localScale -= new Vector3(.45f, .45f, 0);
+    }
+}
---
+++ starWatcher/Assets/Script/starController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class starController : MonoBehaviour
+{
+
+    // Use this for initialization
+    public Camera mainCam;
+    private int count;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            //check to see if user clicks
+        {
+            Ray ray = mainCam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+                //if the raycast hits something
+            {
+                if (hit.transform.tag == "star")
+                    //check to see if its a star
+                {
+                    Debug.Log("My object is clicked by mouse");
+                    hit.transform.tag = "clicked";
+                    if (hit.transform.parent.name == "starGroup1")
+                    {
+                        //if it is part of the first stargroup/picture, add to count.
+                        //whenever the count of stars that makes the pic is satisfied, then we will make
+                        //the picture appear
+                        count += 1;
+                    }
+                }
+                else if (hit.transform.tag == "clicked")
+                    //dont preform the above actions if star is already clicked.
+                {
+                    Debug.Log("Already been clciked yo.");
+                }
+            }
+
+        }
+
+        if (count == 6) {
+            print("star group 1 has all been clicked!");
+        }
+
+    }
+}
